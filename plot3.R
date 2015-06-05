2833d87963fd35e2a163c2a6e76103aec7db7dd1
Nadya Polidorova <moooncancer@gmail.com>
2015-06-06 00:24:49 +0300

    * plot3.R - legend drawn with smaller font size, all code put into a parameterized function * plot3.png

--- plot3.R
+++ plot3.R
@@ -1,35 +1,41 @@
-
 # plot3 -------------------------------------------------------------------
-plot3 <- function() {
+## These parameter values are well-suited for 480x480 resolution.
+## Roughly, cex.legend determines the width of the legend box,
+## while cex.text determines the font size of the labels inside of it.  
+## (see the example code of right-justifying a set of labels
+## provided in graphics::legend docs)
+plot3 <- function(cex.legend=.6, 
+                  cex.text=1.3,
+                  bty='o') {
+      ## download and unpack the data if needed, make a table
+      if (!exists("dataPrepared", mode="logical"))
+            dataPrepared <<- FALSE
+      if (!dataPrepared){
+            source("./prepareData.R")
+            prepareData()      
+      }
       plot(rep(hpc.table$DateTime,3), 
            c(hpc.table$Sub_metering_1,
              hpc.table$Sub_metering_2,
              hpc.table$Sub_metering_3),
            type='n', 
-           xlab="", ylab="Energy sub metering")
+           xlab="", ylab="Energy sub metering",
+           cex.lab=.9, cex.axis=.9)
       lines(hpc.table$DateTime, hpc.table$Sub_metering_1)
       lines(hpc.table$DateTime, hpc.table$Sub_metering_2, col="red")
       lines(hpc.table$DateTime, hpc.table$Sub_metering_3, col="blue")
       
-      ## making the legend more compact than the standard one
-      ## without reducing font size too much
-      ## (see an example code of right-justifying a set of labels
-      ## provided in graphics::legend docs)
-      text.width <- strwidth("   Sub_metering_1", cex=0.8)
+      text.width <- strwidth(" Sub_metering_1", 
+                             cex=cex.legend*cex.text*0.86)
       box <- legend("topright", legend = c(" "," "," "),
              col=c("black", "red", "blue"), 
-             y.intersp=0.7, lty = 1, text.width=text.width,
-             cex=0.67)
+             y.intersp=cex.legend*1.6, lty = 1, text.width=text.width,
+             cex=cex.legend, bty=bty)
       text(x=box$text$x, y=box$text$y,
            c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-           col="black", adj=c(0,0.5))
+           col="black", adj=c(0,0.5),cex=cex.legend*cex.text)
       
       # save to png file --------------------------------------------------
-      dev.copy(png, file="plot3.png")
+      dev.copy(png, file="./plot3.png")
       dev.off()
 }
-
-## download and unpack the data if needed, make a table
-source("./prepareData.R")
-prepareData()
-plot3()
