66b125f971c55b22e11d3b1677317f0140e7ec5a
Nadya Polidorova <moooncancer@gmail.com>
2015-06-06 00:28:24 +0300

    * plot1.R - all the code put into a single function, cex parameters set * plot1.png

--- plot1.R
+++ plot1.R
@@ -1,17 +1,19 @@
 # plot the histogram -------------------------------------------------------
 plot1 <- function() {
+      ## download and unpack the data if needed, make a table
+      if (!exists("dataPrepared", mode="logical"))
+            dataPrepared <<- FALSE
+      if (!dataPrepared){
+            source("./prepareData.R")
+            prepareData()      
+      }            
       hist(hpc.table$Global_active_power, col="red",
       xlab = "Global Active Power (kilowatts)",
-      main = "Global Active Power")
+      main = "Global Active Power", 
+      cex.lab=.9, cex.axis=.9, cex.main=.9)
 
       # save to png file --------------------------------------------------------
 
-      dev.copy(png, file="plot1.png")
+      dev.copy(png, file="./plot1.png")
       dev.off()
 }
-
-## download and unpack the data if needed, make a table
-source("./prepareData.R")
-prepareData()
-plot1()
-
